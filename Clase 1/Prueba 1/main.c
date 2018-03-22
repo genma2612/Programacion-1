9dd8a65e78bdff960ca59acd7501263978595197
genma2612 <manuelagustingonzalez@gmail.com>
2018-03-22 10:50:00 -0300

    1er Commit

---
+++ Clase 1/Prueba 1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    datos numeros enteros: int (4, 5, -6)
+    datos numeros decimales: float (2,5; -1,45)
+    dato caracter: char
+    declaracion de variable: int variable1 --- variable1 = 1
+    */
+    int numero1;
+    float numero2;
+    numero1 = 1;
+    numero2 = 5.6;
+    printf("El 1er numero es: %d y el 2do es: %.1f\n",numero1,numero2);
+    printf("Hello world!\n");
+    return 0;
+}
